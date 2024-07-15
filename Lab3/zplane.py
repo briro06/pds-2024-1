5b07e7f1b9ac823c75deb799a5390df03015dac0
briro06 <brian.moralesl@pucp.edu.pe>
2024-07-15 17:08:18 -0500

    pds

---
+++ Lab3/zplane.py
@@ -0,0 +1,52 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+from  matplotlib import patches
+from matplotlib.figure import Figure
+from matplotlib import rcParams
+    
+def zplane(b,a,filename=None):
+
+    ax = plt.subplot(111)
+    uc = patches.Circle((0,0), radius=1, fill=False, color='black', ls='dashed')
+    ax.add_patch(uc)
+    
+    if np.max(b) > 1:
+        kn = np.max(b)
+        b = b/float(kn)
+    else:
+        kn = 1
+
+    if np.max(a) > 1:
+        kd = np.max(a)
+        a = a/float(kd)
+    else:
+        kd = 1
+        
+    p = np.roots(a)
+    z = np.roots(b)
+    k = kn/float(kd)
+    
+    t1 = plt.plot(z.real, z.imag, 'go', ms=10)
+    plt.setp( t1, markersize=10.0, markeredgewidth=1.0,
+              markeredgecolor='k', markerfacecolor='g')
+
+    t2 = plt.plot(p.real, p.imag, 'rx', ms=10)
+    plt.setp( t2, markersize=12.0, markeredgewidth=3.0,
+              markeredgecolor='r', markerfacecolor='r')
+
+    ax.spines['left'].set_position('center')
+    ax.spines['bottom'].set_position('center')
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+
+    r = 1.5; plt.axis('scaled'); plt.axis([-r, r, -r, r])
+    ticks = [-1, -.5, .5, 1]; plt.xticks(ticks); plt.yticks(ticks)
+
+    if filename is None:
+        plt.show()
+    else:
+        plt.savefig(filename)
+    
+
+    return z, p, k
